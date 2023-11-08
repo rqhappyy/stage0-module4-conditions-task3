75d627302c6f9deb2960d1348237d79b90f06cf8
rqhappyy <dtereschenko110@gmail.com>
2023-11-08 15:46:21 +0300

    task2

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,14 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        if(salary <= 10000){
-            System.out.println(salary - salary*0.15);
+        if (salary <= 0) {
+            System.out.println("wrong input!");
         } else if (salary>10000 && salary<=20000) {
             System.out.println(salary - salary*0.18);
         } else if (salary > 20000) {
             System.out.println(salary - salary*0.2);
-        } else if (salary <= 0) {
-            System.out.println("wrong input!");
+        } else if (salary <= 10000) {
+            System.out.println(salary - salary*0.15);
         }
     }
 }
